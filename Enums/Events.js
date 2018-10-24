71f8fd4252c9b7fc72edd6465120c8c22a8b83d2
Gregers Boye-Jacobsen <gbj@cego.dk>
2018-10-24 13:58:59 +0200

    Added socket-communication between frontend and sorter

---
+++ Enums/Events.js
@@ -0,0 +1,18 @@
+class Events {
+    /**
+     * @return {{CONNECTION: string, MESSAGE: string, STATUS: string}}
+     */
+    static get Socket() {
+        return {
+            CONNECTION: `connection`,
+            MESSAGE: `message`,
+            STATUS: `status`
+        };
+    }
+
+    static get ZeroMQ(){
+        return {}
+    }
+}
+
+module.exports = Events;
