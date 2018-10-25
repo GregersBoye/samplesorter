d595defad034f66d75cbb22e72524a99d1007d49
Gregers Boye-Jacobsen <gbj@cego.dk>
2018-10-25 22:01:40 +0200

    Added zmq-messaging

---
+++ sorter/src/Utils/Logger.js
@@ -0,0 +1,51 @@
+const Logger = require(`./Loggers/Winston`);
+
+/**
+ * There's a bug in winston causing meta-data to be wrongly attached, so therefore this wrapper
+ * Also it limits what logging-levels are available.
+ */
+
+class Log {
+    /**
+     * Log a message with severity-level debug
+     *
+     * @param {string} message
+     * @param {*} meta
+     */
+    static debug(message, meta) {
+        Logger.debug(message, {meta: meta});
+    }
+
+    /**
+     * Log a message with severity-level warning
+     *
+     * @param {string} message - The message
+     * @param {*} meta
+     */
+    static warning(message, meta) {
+        Logger.warning(message, {meta: meta});
+    }
+
+    /**
+     * Log a message with severity-level error
+     *
+     * @param {string} message - The message
+     * @param {*[]} meta
+     */
+    static err(message, meta) {
+        Logger.error(message, {meta: meta});
+
+    }
+
+    /**
+     * Log a message with severity-level info
+     *
+     * @param {string} message
+     * @param {*} meta
+     */
+    static info(message, meta) {
+        Logger.info(message, {meta: meta});
+    }
+}
+
+module.exports = Log;
